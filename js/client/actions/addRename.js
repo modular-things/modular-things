b7b81b90428715f058d669aaa968f845e1aba92a
Jake <jake.read@cba.mit.edu>
2022-10-31 22:34:40 -0400

    can now rename modular things, but needs firmware-name and instance-name separation, or something

--- js/client/actions/addRename.js
+++ js/client/actions/addRename.js
@@ -1,7 +1,7 @@
 export async function addRename(thing, osap) {
   thing.rename = async (name) => {
     try {
-      await osap.mvc.renameVertex(thing.vt.route, name)
+      await osap.mvc.renameVertex(thing.vt.route, "rt_" + name)
     } catch (err) {
       console.error(err)
     }
