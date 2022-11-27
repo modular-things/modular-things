9f813e8414aa6c7e3dc07427ab815ed0209b05b3
Quentin Bolsee <quentinbolsee@hotmail.com>
2022-11-26 22:22:44 -0500

    TOF and accelerometer

--- arduino/time-of-flight/time-of-flight.ino
+++ arduino/time-of-flight/time-of-flight.ino
@@ -2,9 +2,13 @@
 #include <vt_endpoint.h>
 #include <vp_arduinoSerial.h>
 #include <core/ts.h>
+#include <Wire.h>
+#include <VL53L1X.h> // https://www.arduino.cc/reference/en/libraries/vl53l1x/
+
+VL53L1X sensor;
 
 // type of board (firmware name)
-OSAP osap("time-of-flight");
+OSAP osap("timeOfFlight");
 
 // ---------------------------------------------- 0th Vertex: OSAP USB Serial
 VPort_ArduinoSerial vp_arduinoSerial(&osap, "usbSerial", &Serial);
@@ -14,21 +18,28 @@ boolean preTOFQuery(void);
 
 Endpoint tofEndpoint(&osap, "tofQuery", preTOFQuery);
 
-Endpoint tofEndpoint = osap.endpoint("tofQuery");
-
 boolean preTOFQuery(void) {
-  uint8_t buf[N_PAD * 2];
-  uint16_t wptr = 0;
-  for(uint8_t p = 0; p < N_PAD; p ++){
-    ts_writeUint16(qt_array[p].measure(), buf, &wptr);
-  }
-  tofEndpoint.write(buf, N_PAD * 2);
+  uint8_t buf[2];
+  sensor.read();
+  uint16_t value = sensor.ranging_data.range_mm;
+  buf[0] = value & 0xFF;
+  buf[1] = value >> 8 & 0xFF;
+  tofEndpoint.write(buf, 2);
   return true;
 }
 
 void setup() {
   osap.init();
   vp_arduinoSerial.begin();
+
+  Wire.begin();
+  Wire.setClock(400000); // 400 KHz I2C
+  sensor.setTimeout(500);
+  sensor.init();
+
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
+  sensor.startContinuous(50);
 }
 
 void loop() {
