86e40dbbce2d859ba6838a58f2689c7bd10b52c1
Jake <jake.read@cba.mit.edu>
2022-11-29 08:56:28 -0500

    start examples

---
+++ js/examples/circleMachine.js
@@ -0,0 +1,15 @@
+let machine = createMachine([ymotor, xmotor])
+console.log(machine)
+
+ymotor.setCScale(0.55)
+ymotor.setSPU(20)
+xmotor.setCScale(0.55)
+xmotor.setSPU(20)
+
+// in rads 
+let radius = 100
+for(let i = 0; i < 100; i += 0.1){
+  let coord = [Math.sin(i) * radius, Math.cos(i) * radius]
+  console.log(coord)
+  await machine.absolute(coord, 1000, 2000)
+}
