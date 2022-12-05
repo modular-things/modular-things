019e09f71d530e83df5bc28f9089aa4b3ac697e4
Quentin Bolsee <quentinbolsee@hotmail.com>
2022-12-05 15:37:24 -0500

    stepper button callback

--- js/virtualThings/capacitive.js
+++ js/virtualThings/capacitive.js
@@ -1,5 +1,5 @@
 /*
-a "virtual thing" - of course 
+a "virtual thing" - of course
 
 Jake Read, Leo McElroy and Quentin Bolsee at the Center for Bits and Atoms
 (c) Massachusetts Institute of Technology 2022
@@ -17,27 +17,27 @@ let N_PADS = 6
 
 export default function(osap, vt, name) {
 
-  // ---------------------------------- OSAP... stuff, 
+  // ---------------------------------- OSAP... stuff,
   let routeToFirmware = PK.VC2VMRoute(vt.route)
 
   // this is the '1th' vertex, so we address it like-this:
   let rgbEndpointMirror = osap.endpoint("rgbEndpointMirror")
   rgbEndpointMirror.addRoute(PK.route(routeToFirmware).sib(1).end());
 
-  // ahn query mechanism, we just use this software handle to write 
-  // endpoint-query packets... to whatever is at the given route 
+  // ahn query mechanism, we just use this software handle to write
+  // endpoint-query packets... to whatever is at the given route
   let padQuery = osap.query(PK.route(routeToFirmware).sib(2).end());
 
   // we should have a setup function:
   const setup = async () => {
-    // though we sometimes don't need it... esp if all direct-write 
+    // though we sometimes don't need it... esp if all direct-write
   }
 
   return {
     setRGB: async (r, g, b) => {
       try {
         // float, float, float, -> int-etc,
-        // we could also do the i.e. linearization here, or accept various "color" types 
+        // we could also do the i.e. linearization here, or accept various "color" types
         let datagram = new Uint8Array(3)
         datagram[0] = 255 - r * 255
         datagram[1] = 255 - g * 255
@@ -50,16 +50,16 @@ export default function(osap, vt, name) {
     },
     readPad: async (index) => {
       try {
-        // just get 'em all 
+        // just get 'em all
         let data = await padQuery.pull();
         let vals = []
         for(let p = 0; p < N_PADS; p ++){
           vals.push(TS.read("uint16", data, p * 2))
         }
         // then return the most recent,
-        return vals[index]/1028
-        // or we could have folks give .readPad() multiple indices, returning each in order, 
-        // or it could just always return the full set, etc... 
+        return vals[index]/1023
+        // or we could have folks give .readPad() multiple indices, returning each in order,
+        // or it could just always return the full set, etc...
       } catch (err) {
         console.error(err)
       }
--- js/virtualThings/stepper.js
+++ js/virtualThings/stepper.js
@@ -1,6 +1,6 @@
 /*
 stepper.js
-a "virtual thing" - of course 
+a "virtual thing" - of course
 Jake Read, Leo McElroy and Quentin Bolsee at the Center for Bits and Atoms
 (c) Massachusetts Institute of Technology 2022
 This work may be reproduced, modified, distributed, performed, and
@@ -13,49 +13,77 @@ import PK from "../osapjs/core/packets.js"
 import { TS } from "../osapjs/core/ts.js"
 
 export default function stepper(osap, vt, name) {
-  // the "vt.route" goes to our partner's "root vertex" - but we 
+  // local state
+  let onButtonStateChangeHandler = (state) => {
+    console.warn(`default button state change in ${name}, to ${state}`);
+  }
+
+  // the "vt.route" goes to our partner's "root vertex" - but we
   // want to address relative siblings, so I use this utility:
   let routeToFirmware = PK.VC2VMRoute(vt.route)
-  // here we basically write a "mirror" endpoint for each downstream thing, 
-  // -------------------------------------------- 1: target data 
-  // now I can index to the 1st endpoint (I know it's this one because 
+  // here we basically write a "mirror" endpoint for each downstream thing,
+  // -------------------------------------------- 1: target data
+  // now I can index to the 1st endpoint (I know it's this one because
   // I wrote the firmware!) just by adding a .sib() to that route;
   let targetDataEndpoint = osap.endpoint(`targetDataMirror_${name}`)
   targetDataEndpoint.addRoute(PK.route(routeToFirmware).sib(1).end())
   // -------------------------------------------- 2: motion state is a query object:
   let motionStateQuery = osap.query(PK.route(routeToFirmware).sib(2).end())
-  // -------------------------------------------- 3: set current position 
+  // -------------------------------------------- 3: set current position
   let positionSetEndpoint = osap.endpoint(`setPositionMirror_${name}`)
   positionSetEndpoint.addRoute(PK.route(routeToFirmware).sib(3).end())
-  // -------------------------------------------- 4: settings, 
+  // -------------------------------------------- 4: settings,
   let settingsEndpoint = osap.endpoint(`settingsMirror_${name}`)
   settingsEndpoint.addRoute(PK.route(routeToFirmware).sib(4).end())
-  // -------------------------------------------- 5: a button, not yet programmed 
-
-  // -------------------------------------------- we need a setup, 
+  // -------------------------------------------- 5: a button, not yet programmed
+  let buttonRxEndpoint = osap.endpoint(`buttonCatcher_${name}`)
+  buttonRxEndpoint.onData = (data) => {
+    onButtonStateChangeHandler(data[0] > 0 ? true : false);
+  }
+  // -------------------------------------------- we need a setup,
   const setup = async () => {
-    // erp, but this firmware actually is all direct-write, nothing streams back 
+    // erp, but this firmware actually is all direct-write, nothing streams back
+    try {
+      // we want to hook i.e. our button (in embedded, at index 2) to our button rx endpoint,
+      // whose index we can know...
+      // given that we know ~ what the topology looks like in these cases (browser...node...usb-embedded)
+      // we should be able to dead-reckon the route up:
+      let routeUp = PK.route().sib(0).pfwd().sib(0).pfwd().sib(buttonRxEndpoint.indice).end()
+      // the source of our button presses is here... the 2nd endpoint at our remote thing
+      let source = vt.children[5]
+      // rm any previous,
+      try {
+        await osap.mvc.removeEndpointRoute(source.route, 0)
+      } catch (err) {
+        // this is chill, we get an error if we try to delete and nothing is there, can ignore...
+        // console.error(err)
+      }
+      // so we build a route from that thing (the source) to us, using this mvc-api:
+      await osap.mvc.setEndpointRoute(source.route, routeUp)
+    } catch (err) {
+      throw err
+    }
   }
 
   // -------------------------------------------- Setters
-  // how many steps-per-unit, 
-  // this could be included in a machineSpaceToActuatorSpace transform as well, 
+  // how many steps-per-unit,
+  // this could be included in a machineSpaceToActuatorSpace transform as well,
   let spu = 20
-  // each has a max-max velocity and acceleration, which are user settings, 
-  // but velocity is also abs-abs-max'd at our tick rate... 
+  // each has a max-max velocity and acceleration, which are user settings,
+  // but velocity is also abs-abs-max'd at our tick rate...
   let absMaxVelocity = 4000 / spu
   let absMaxAccel = 10000
   let lastVel = absMaxVelocity
-  let lastAccel = 100             // units / sec 
+  let lastAccel = 100             // units / sec
 
   let setPosition = async (pos) => {
     try {
-      // halt... this also mode-swaps to VEL, so when we set a new posn' 
-      // the motor won't slew to it 
+      // halt... this also mode-swaps to VEL, so when we set a new posn'
+      // the motor won't slew to it
       await stop()
-      // write up a new-position-paquet, 
+      // write up a new-position-paquet,
       let datagram = new Uint8Array(4)
-      let wptr = 0 
+      let wptr = 0
       wptr += TS.write("float32", pos, datagram, wptr)
       await positionSetEndpoint.write(datagram, "acked")
     } catch (err) {
@@ -76,7 +104,7 @@ export default function stepper(osap, vt, name) {
   let setAbsMaxAccel = (maxAccel) => { absMaxAccel = maxAccel }
 
   let setAbsMaxVelocity = (maxVel) => {
-    // not beyond this tick-based limit, 
+    // not beyond this tick-based limit,
     if (maxVel > 4000 / spu) {
       maxVel = 4000 / spu
     }
@@ -88,29 +116,29 @@ export default function stepper(osap, vt, name) {
       let datagram = new Uint8Array(4)
       let wptr = 0
       wptr += TS.write("float32", cscale, datagram, wptr)  // it's 0-1, firmware checks
-      // and we can shippity ship it, 
+      // and we can shippity ship it,
       await settingsEndpoint.write(datagram, "acked")
     } catch (err) {
       console.error(err)
     }
   }
 
-  // tell me about your steps-per-unit, 
-  // note that FW currently does 1/4 stepping: 800 steps / revolution 
+  // tell me about your steps-per-unit,
+  // note that FW currently does 1/4 stepping: 800 steps / revolution
   let setSPU = (_spu) => {
     spu = _spu
     if (absMaxVelocity > 4000 / spu) { absMaxVelocity = 4000 / spu }
-    // we know that we have a maximum steps-per-second of 4000, so we can say 
+    // we know that we have a maximum steps-per-second of 4000, so we can say
     console.warn(`w/ spu of ${spu}, this ${name} has a new abs-max velocity ${absMaxVelocity}`)
   }
 
-  // -------------------------------------------- Getters 
+  // -------------------------------------------- Getters
 
   // get states
   let getState = async () => {
     try {
       let data = await motionStateQuery.pull()
-      // deserialize... 
+      // deserialize...
       return {
         pos: TS.read("float32", data, 0) / spu,
         vel: TS.read("float32", data, 4) / spu,
@@ -143,9 +171,9 @@ export default function stepper(osap, vt, name) {
   let getAbsMaxVelocity = () => { return absMaxVelocity }
   let getAbsMaxAccel = () => { return absMaxAccel }
 
-  // -------------------------------------------- Operative 
+  // -------------------------------------------- Operative
 
-  // await no motion, 
+  // await no motion,
   let awaitMotionEnd = async () => {
     try {
       return new Promise(async (resolve, reject) => {
@@ -169,66 +197,66 @@ export default function stepper(osap, vt, name) {
   // sets the position-target, and delivers rates, accels to use while slewing-to
   let target = async (pos, vel, accel) => {
     try {
-      // modal vel-and-accels, and guards 
+      // modal vel-and-accels, and guards
       vel ? lastVel = vel : vel = lastVel;
       accel ? lastAccel = accel : accel = lastAccel;
       if (accel > absMaxAccel) { accel = absMaxAccel; lastAccel = accel; }
       if (vel > absMaxVelocity) { vel = absMaxVelocity; lastVel = vel; }
-      // also, warn against zero-or-negative velocities & accelerations 
+      // also, warn against zero-or-negative velocities & accelerations
       if (vel <= 0 || accel <= 0) throw new Error(`y'all are trying to go somewhere, but modal velocity or accel are negative, this won't do...`)
-      // stuff a packet, 
+      // stuff a packet,
       let datagram = new Uint8Array(13)
       let wptr = 0
-      datagram[wptr++] = 0 // MOTION_MODE_POS 
-      // write pos, vel, accel *every time* and convert-w-spu on the way out, 
+      datagram[wptr++] = 0 // MOTION_MODE_POS
+      // write pos, vel, accel *every time* and convert-w-spu on the way out,
       wptr += TS.write("float32", pos * spu, datagram, wptr)  // write posn
       wptr += TS.write("float32", vel * spu, datagram, wptr)  // write max-vel-during
       wptr += TS.write("float32", accel * spu, datagram, wptr)  // write max-accel-during
-      // and we can shippity ship it, 
+      // and we can shippity ship it,
       await targetDataEndpoint.write(datagram, "acked")
     } catch (err) {
       console.error(err)
     }
   }
 
-  // goto-this-posn, using optional vel, accel, and wait for machine to get there 
+  // goto-this-posn, using optional vel, accel, and wait for machine to get there
   let absolute = async (pos, vel, accel) => {
     try {
-      // sets motion target, 
+      // sets motion target,
       await target(pos, vel, accel)
-      // then we could do... await-move-done ? 
+      // then we could do... await-move-done ?
       await awaitMotionEnd()
       console.log(`abs move to ${pos} done`)
     } catch (err) {
       console.error(err)
     }
-  } // end absolute 
+  } // end absolute
 
-  // goto-relative, also wait, 
+  // goto-relative, also wait,
   let relative = async (delta, vel, accel) => {
     try {
       let state = await getState()
       let pos = delta + state.pos
-      // that's it my dudes, 
+      // that's it my dudes,
       await absolute(pos, vel, accel)
     } catch (err) {
       console.error(err)
     }
   }
 
-  // goto-this-speed, using optional accel, 
+  // goto-this-speed, using optional accel,
   let velocity = async (vel, accel) => {
     try {
-      // modal accel, and guards... 
+      // modal accel, and guards...
       accel ? lastAccel = accel : accel = lastAccel;
       if (accel > absMaxAccel) { accel = absMaxAccel; lastAccel = accel; }
       if (vel > absMaxVelocity) { vel = absMaxVelocity; lastVel = vel; }
-      // note that we are *not* setting last-vel w/r/t this velocity... esp. since we often call this 
-      // w/ zero-vel, to stop... 
-      // now write the paquet, 
+      // note that we are *not* setting last-vel w/r/t this velocity... esp. since we often call this
+      // w/ zero-vel, to stop...
+      // now write the paquet,
       let datagram = new Uint8Array(9)
       let wptr = 0
-      datagram[wptr++] = 1 // MOTION_MODE_VEL 
+      datagram[wptr++] = 1 // MOTION_MODE_VEL
       wptr += TS.write("float32", vel * spu, datagram, wptr)  // write max-vel-during
       wptr += TS.write("float32", accel * spu, datagram, wptr)  // write max-accel-during
       // mkheeeey
@@ -248,7 +276,7 @@ export default function stepper(osap, vt, name) {
     }
   }
 
-  // we return fns that user can call, 
+  // we return fns that user can call,
   return {
     // operate w/
     target,
@@ -257,7 +285,7 @@ export default function stepper(osap, vt, name) {
     velocity,
     stop,
     awaitMotionEnd,
-    // setters... 
+    // setters...
     setPosition,
     setVelocity,
     setAccel,
@@ -265,73 +293,80 @@ export default function stepper(osap, vt, name) {
     setAbsMaxVelocity,
     setCScale,
     setSPU,
-    // inspect... 
+    // inspect...
     getPosition,
     getVelocity,
     getAbsMaxVelocity,
     getAbsMaxAccel,
-    // these are hidden 
+    onButtonStateChange: (fn) => { onButtonStateChangeHandler = fn; },
+    // these are hidden
     setup,
     vt,
     api: [
-      { 
+      {
         name: "absolute",
         args: [
           "pos: [x, y, z]",
         ]
       },
-      { 
+      {
         name: "relative",
         args: [
           "pos: [x, y, z]",
         ]
       },
-      { 
+      {
         name: "setVelocity",
         args: [
           "number",
         ]
       },
-      { 
+      {
         name: "setAccel",
         args: [
           "number",
         ]
       },
-      { 
+      {
         name: "stop",
         args: []
       },
-      { 
+      {
         name: "awaitMotionEnd",
         args: []
       },
-      { 
+      {
         name: "getState",
         args: [],
         return: `
-          { 
-            pos: [x, y, z], 
-            vel: number, 
-            accel: number 
+          {
+            pos: [x, y, z],
+            vel: number,
+            accel: number
           }
         `
       },
-      { 
+      {
         name: "getAbsMaxVelocity",
         args: [],
-        return: "number", 
+        return: "number",
       },
-      { 
+      {
         name: "setCScale",
         args: [
           "number",
         ]
       },
-      { 
+      {
         name: "setSPU",
         args: [
           "number",
+        ]
+      },
+      {
+        name: "onButtonStateChange",
+        args: [
+          "function: (buttonState) => {}"
         ]
       }
     ]
