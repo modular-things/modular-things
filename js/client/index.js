219fb49ee0645c513777e8e7586d2a5f98774dc5
Jake <jake.read@cba.mit.edu>
2022-12-06 19:53:44 -0500

    unfk- last borked commit

--- js/client/index.js
+++ js/client/index.js
@@ -1,122 +1,227 @@
-// aMallet, aMotor, 
-// bMallet, bMotor, 
+import { rescan } from "./modularThingClient.js";
+import { global_state } from "./global_state.js";
+import createSynchronizer from "../virtualThings/synchronizer";
+import Terminal from "xterm";
+import { render, html } from "lit-html";
 
-// let's calculate the steps-per-unit, 
-let circ = 64.5
-// with 800 steps per revolution, 
-let spu = 800 / circ
+import "./codemirror.js";
 
-let motors = [aMotor, bMotor]
+const view = (state) => html`
+  <div class="menu">
+    <div class="menu-item" @click=${runCode}>run (shift+enter)</div>
+    <div class="menu-item" @click=${rescan}>scan</div>
+    <div class="menu-item" @click=${() => global_state.viewWindow = !state.viewWindow }>view/code</div>
+  </div>
+  <div class="content">
+    <div class="left-pane">
+   
+      <codemirror-editor></codemirror-editor>
+      <div class="terminal">
+        <div class="entry-line">\>\>\><input/></div>
+      </div>
+        
+      <!-- <textarea spellcheck="false" class="code-editor"></textarea> -->
+    </div>
+    <div class="things">
+      <div style="font-weight:700; font-size: 1.2em; padding-bottom: 10px;">List of Things</div>
+      ${Object.entries(global_state.things).map(drawThing)}
+    </div>
 
-for(let m of motors){
-  await m.setPosition(0)
-  await m.setStepsPerUnit(spu)
-  await m.setCurrentScale(0.75)
-  await m.setAccel(10000)
-  await m.setVelocity(500)
-}
+    <div class=${["view-window", state.viewWindow ? "" : "hide"].join(" ")}>Test</div>
+  </div>
+  ${state.renaming !== "" ? renameForm(state) : ""}
+`
 
-let mallets = [aMallet, bMallet]
+const drawThing = (thing) => html`
+  <div class="thing">
+    <div class="thing-top-line">
+      <div class="thing-name">Name: ${thing[0]}</div>
+      <button class="button" @click=${() => global_state.renaming = thing[0] }>rename</button>
+    </div>
+    <div>Type: ${thing[1].firmwareName}</div>
+    <div class="thing-api">${drawApi(thing)}</div>
+  </div>
+  <hr/>
+`
 
-// in 1/16 time base (allegedly) 
-let song = [
-  [0, 1],
-  [0, 1],
-  [1, 1],
-  [0, 1],
-  [6, 1],
-  [6, 1],
-  [6, 1],
-  [0, 1],
-  [7, 1],
-  [0, 1],
-  [6, 1],
-  [0, 1],
-  [5, 1],
-  [5, 1],
-  [5, 1],
-]
+const drawApi = (thing) => {
+  const [ name, obj ] = thing;
+  const api = obj.vThing.api;
 
-let noteInterval = 47.85 / 2
-let notes = [
-  //  0,    1     2     3     4     5     6     7     8     9     10    11    12    13    
-      0,    "C",  "D",  "E",  "F",  "G",  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "A"
-]
+  
 
-// for (let n = 0; n < notes.length; n++) {
-//   notes[n] = {
-//     letter: notes[n],
-//     pos: noteInterval * n
-//   }
-// }
-
-let thwapNote = async (index, m = 0) => {
-  try {
-    if (index >= notes.length) throw new Error('u r tryna thwap out of bounds')
-    await motors[m].absolute(notes[index].pos)
-    await mallets[m].setGate(1)
-    setTimeout(async () => {
-      await mallets[m].setGate(0)
-    }, 1)
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-let gotoNote = async (index, m = 0) => {
-  try {
-    if (index >= notes.length) throw new Error('u r tryna go out of bounds')
-    if (m >= motors.length) throw new Error('again w/ the out of bounds operation')
-    await motors[m].absolute(notes[index].pos)
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-let thwap = async (m = 0, op = true) => {
-  try {
-    if(!op) return 
-    await mallets[m].setGate(1)
-    setTimeout(async () => {
-      await mallets[m].setGate(0)
-    }, 1)
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-let runTimeSlot = async (slot) => {
-  try {
-    let notes = song[slot]
-    if(!notes) throw new Error("we've gone beyond the song end, I think")
-    // thwap if they exist: we should be already-at-the-place, 
-    await Promise.all([thwap(0, notes[0]), thwap(1, notes[1])])
-    // now look ahead to next positions ? 
-    let scheduled = [false, false]
-    for(let s = slot + 1; s < song.length; s ++){
-      for(let m = 0; m < 2; m ++){
-        if(song[s][m] && !scheduled[m]){
-          scheduled[m] = true 
-          let note = song[s][m]
-          motors[m].absolute((note - 1) * noteInterval)
-        }
+  return  api.map( entry => html`
+    <div class="apiEntry">
+      <div>${entry.name}(${entry.args.map(x => x.split(":")[0]).join(", ")})</div>
+      ${entry.args.map((x, i) => html`<div style="padding-left: 10px;">${x}</div>`)}
+      ${ entry.return 
+          ? html`<div class="apiEntry-return"><b>returns:</b> ${entry.return}</div>`
+          : ""
       }
+    </div>
+  ` );
+}
+
+const getApi = (thing) => {
+  const api = Object.keys(thing).map( x => [ x, getParamNames(thing[x]) ]);
+  // don't include "setup" or "setName" or "vt"
+  return api
+    .filter(x => !["setup", "setName", "vt", "firmwareName"].includes(x[0]))
+    .map(apiEntry);
+}
+
+const apiEntry = ([name, params]) => html`
+  <div class="apiEntry">${name}(${params.join(", ")})</div>
+`
+
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var ARGUMENT_NAMES = /([^\s,]+)/g;
+function getParamNames(func) {
+  var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+  if(result === null)
+     result = [];
+  return result;
+}
+
+const renameForm = (state) => html`
+  <div class="rename-form">
+    <div class="rename-input">
+      <div class="rename-label">Name:</div>
+      <input .value=${state.renaming} class="rename-form-input"/>
+    </div>
+    <button class="button" @click=${() => {
+      // console.log(state);
+      const thing = state.things[state.renaming];
+      const newName = document.querySelector(".rename-form-input").value;
+      thing.vThing.setName(newName);
+      delete state.things[state.renaming];
+      state.things[newName] = thing;
+      state.renaming = "";
+    }}>rename</button>
+    <button class="button" @click=${() => state.renaming = ""}>close</button>
+  </div>
+`
+
+const r = () => {
+  render(view(global_state), document.body);
+  window.requestAnimationFrame(r);
+};
+
+window.state = () => console.log(global_state);
+
+function init() {
+  r();
+  // "terminal is not a constructor" despite NPM reinstall 
+  // var term = new Terminal();
+  // term.open(document.querySelector('.terminal'));
+  // term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+  const cache = localStorage.getItem('cache');
+  if (cache) {
+    const cm = document.querySelector("codemirror-editor");
+    cm.view.dispatch({
+      changes: { from: 0, insert: cache ?? "" }
+    });
+  }
+
+
+}
+
+init();
+
+function getCode() {
+  const cm = document.querySelector("codemirror-editor");
+  const doc = cm.view.state.doc;
+  const code = doc.toString();
+
+  return code;
+}
+
+let intervals = [];
+let timeouts = [];
+let loops = [];
+
+function runCode(e) {
+  const code = getCode();
+
+  const AsyncFunction = (async function () {}).constructor;
+
+  intervals.forEach(clearInterval);
+  timeouts.forEach(clearTimeout);
+  loops.forEach((x, i) => { loops[i] = false });
+
+
+  const patchedInterval = (callback, time, ...args) => {
+    const interval = setInterval(callback, time, ...args);
+    intervals.push(interval);
+    return interval;
+  }
+
+  const patchedTimeout = (callback, time, ...args) => {
+    const timeout = setTimeout(callback, time, ...args);
+    timeouts.push(timeout);
+    return timeout;
+  }
+
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+  const loop = async (fn, minterval = 0) => {
+    let n = loops.length;
+    loops.push(true);
+    while (loops[n]) {
+      const date = new Date();
+      const start = date.getTime();
+      await fn();
+      const elapsed = (date.getTime()) - start;
+      if (elapsed < minterval) await sleep(minterval - elapsed);
     }
-  } catch (err) {
-    console.error(err)
   }
+
+  const render = (node) => {
+    const viewWindow = document.querySelector(".view-window");
+    viewWindow.innerHTML = "";
+    viewWindow.append(node);
+  }
+
+  const things = {};
+
+  for (const key in global_state.things) {
+    things[key] = global_state.things[key].vThing;
+  }
+
+  const args = {
+    ...things,
+    setInterval: patchedInterval,
+    setTimeout: patchedTimeout,
+    createSynchronizer,
+    loop,
+    render,
+    sleep,
+    // document: null,
+    window: null,
+    eval: null,
+  }
+
+  const names = Object.keys(args);
+  const values = Object.values(args);
+
+  const f = new AsyncFunction(
+    ...names,
+    code
+  );
+
+  f(
+    ...values
+  );
 }
 
-for(let s = 0; s < song.length; s ++){
-  try {
-    console.warn(s)
-    await runTimeSlot(s)
-    await sleep(250)
-  } catch (err){
-    console.error(err)
-    break
-  }
-}
+window.addEventListener("keydown", (e) => {
+  const code = getCode();
 
-await aMotor.absolute(0)
-await bMotor.absolute(0)
+  window.localStorage.setItem("cache", code);
+
+  if (e.keyCode === 13 && e.shiftKey) {
+    runCode();
+    e.preventDefault();
+  }
+})
