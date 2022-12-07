d2f2f8cb205481c55028736106256dd92c1bb2df
Quentin Bolsee <quentinbolsee@hotmail.com>
2022-12-07 06:30:14 -0500

    corexy example

--- js/examples/circleMachine.js
+++ js/examples/circleMachine.js
@@ -1,12 +1,12 @@
-let corexy = createSynchronizer([amotor, bmotor])
+let corexy = createSynchronizer([aMotor, bMotor])
 console.log(corexy)
 
-amotor.setCScale(0.55)
-amotor.setSPU(20)
-bmotor.setCScale(0.55)
-bmotor.setSPU(20)
+aMotor.setCurrentScale(0.55)
+aMotor.setStepsPerUnit(20)
+bMotor.setCurrentScale(0.55)
+bMotor.setStepsPerUnit(20)
 
-// in rads 
+// in rads
 let radius = 25
 for(let i = 0; i < 100; i += 0.2){
   let coord = [Math.sin(i) * radius, Math.cos(i) * radius]
--- js/examples/potentiometerMachine.js
+++ js/examples/potentiometerMachine.js
@@ -1,17 +1,17 @@
-let corexy = createSynchronizer([amotor, bmotor])
+let corexy = createSynchronizer([aMotor, bMotor])
 console.log(corexy)
 
-amotor.setCScale(0.75)
-amotor.setSPU(20)
-bmotor.setCScale(0.75)
-bmotor.setSPU(20)
+aMotor.setCurrentScale(0.75)
+aMotor.setStepsPerUnit(20)
+bMotor.setCurrentScale(0.75)
+bMotor.setStepsPerUnit(20)
 
-amotor.setVelocity(500)
-amotor.setAccel(750)
-bmotor.setVelocity(500)
-bmotor.setAccel(750)
+aMotor.setVelocity(500)
+aMotor.setAccel(750)
+bMotor.setVelocity(500)
+bMotor.setAccel(750)
 
-// let's do... get-set 
+// let's do... get-set
 loop(async () => {
   let posns = await Promise.all([joypot.readPotentiometer(0), joypot.readPotentiometer(1)])
   posns[0] *= 100
