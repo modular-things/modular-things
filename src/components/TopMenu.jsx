f6b649523381566635301a2a0cf35ec0288ef279
leomcelroy <leomcelroy@gmail.com>
2023-02-01 01:49:39 -0500

    moved samples

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/TopMenu.module.css'
 
 import { global_state } from "../lib/global_state"
+import { SAMPLES } from "../lib/SAMPLES"
 
 function setCode(code) {
   const cm = global_state.codemirror;
@@ -8,51 +9,6 @@ function setCode(code) {
   cm.dispatch({
     changes: { from: 0, to: current.length, insert: code }
   });
-}
-
-const SAMPLES = {
-  potLoop:`loop(async () => {
-  const val = await pot.readPotentiometer(0);
-  console.log(val);
-}, 50);
-`,
-  sychronizer: `await motor0.setCurrentScale(0.7);
-await motor0.setStepsPerUnit(200);
-
-await motor1.setCurrentScale(0.7);
-await motor1.setStepsPerUnit(200);
-
-const machine = createSychronizer([motor0, motor1]);
-
-machine.setPosition([0, 0]);
-
-for (let i = 0; i < 6; i++) {
-  await machine.absolute([1, 1]);
-  await delay(1000);
-  await machine.absolute([0, 0]);
-  await delay(1000);
-}
-
-`,
-  renderView: `const el = document.createElement("div");
-
-el.style = \`
-  padding: 10px;
-\`
-
-el.innerHTML = \`
-  <div>hello world!</div>
-  <button>press this</button>
-\`;
-
-el
-  .querySelector("button")
-  .addEventListener("click", () => {
-    console.log("hello");
-  })
-
-render(el);
-`
 }
 
 
