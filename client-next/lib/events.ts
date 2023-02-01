ff8470b87ba67a78eef6b6cb095d1973e57bd134
leomcelroy <leomcelroy@gmail.com>
2023-01-31 23:39:46 -0500

    removing other clients

--- client-next/lib/events.ts
+++
@@ -1,5 +0,0 @@
-import { createEvent } from "niue";
-import { File, Folder, FSNode, FSNodeType } from "./fs";
-
-export const [useOnOpenFile, dispatchOpenFile] = createEvent<File>();
-export const [useOnDeleteNode, dispatchDeleteNode] = createEvent<FSNode>();
