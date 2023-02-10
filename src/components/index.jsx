d47866a0d8958a25016e3e60da08f6ae2d37597b
leomcelroy <leomcelroy@gmail.com>
2023-02-10 11:52:04 -0500

    added file loading

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -25,6 +25,23 @@ export default function Page() {
       changes: { from: 0, insert: cache ?? "" }
     });
 
+    const search = window.location.search;
+    const file = new URLSearchParams(search).get("file");
+    if (file) {
+      let file_url = file;
+      if (!file.startsWith("http")) file_url = `examples/${file}`;
+      fetch(file_url).then(async (res) => {
+        const text = await res.text();
+        
+        const currentProg = cm.state.doc.toString();
+
+        cm.dispatch({
+          changes: { from: 0, to: currentProg.length, insert: text }
+        });
+        
+      });
+    }
+
     initialized = true;
 
 
