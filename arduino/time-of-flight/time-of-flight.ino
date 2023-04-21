2e794a67461bb2afe7b6640d9babaed984fe8c69
Jake <jake.read@cba.mit.edu>
2023-04-21 11:43:10 -0400

    notes on strange time-of-flight bug

--- arduino/time-of-flight/time-of-flight.ino
+++ arduino/time-of-flight/time-of-flight.ino
@@ -9,14 +9,28 @@ OSAP_Gateway_USBSerial serLink(&Serial);
 OSAP_Port_DeviceNames namePort("timeOfFlight");
 
 size_t readDistance(uint8_t* data, size_t len, uint8_t* reply) {
-  sensor.read();
-  uint16_t value = sensor.ranging_data.range_mm;
-  reply[0] = value & 0xFF;
-  reply[1] = value >> 8 & 0xFF;
-  return 2;
+  if(sensor.timeoutOccurred()){
+    reply[0] = 0;
+  } else {
+    sensor.read();
+    uint16_t value = sensor.ranging_data.range_mm;
+    reply[0] = 1;
+    reply[1] = value & 0xFF;
+    reply[2] = value >> 8 & 0xFF;
+  }
+  return 3;
 }
 
 OSAP_Port_Named readDistance_port("readDistance", readDistance);
+
+// TODO: bugfix
+/*
+* some trouble with the board / I2C
+* behaviour is... first time we program the board, it works as expects
+* all subsequent times (unplug, plug back in...) the I2C times out...
+* so... go looking for flash bugs, IDK ? 
+* bug appears whether / not we write a new name to flash... 
+*/
 
 void setup() {
   osap.begin();
