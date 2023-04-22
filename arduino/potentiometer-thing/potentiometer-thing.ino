4863d1f0e3cb66475802bb7d60d873c06d1d4d71
Jake <jake.read@cba.mit.edu>
2023-04-22 18:39:25 +0200

    match rgbb and potentiometer to osap examples

--- arduino/potentiometer-thing/potentiometer-thing.ino
+++ arduino/potentiometer-thing/potentiometer-thing.ino
@@ -1,11 +1,31 @@
 #include <osap.h>
 
+// -------------------------- Define Pins for our two Potentiometers
+
 #define PIN_POT1 8
 #define PIN_POT2 7
 
+// -------------------------- Instantiate the OSAP Runtime, 
+
 OSAP_Runtime osap;
+
+// -------------------------- Instantiate a link layer, 
+// handing OSAP the built-in Serial object to send packetized 
+// data around the network 
+
 OSAP_Gateway_USBSerial serLink(&Serial);
+
+// -------------------------- Adding this software-defined port 
+// allows remote services to find the type-name of this device (here "potentiometer")
+// and to give it a unique name, that will be stored after reset 
+
 OSAP_Port_DeviceNames namePort("potentiometer");
+
+// -------------------------- We can define functions that will be called
+// when messages to their function-name are sent to our device. 
+// if we use `size_t func(uint8_t* data, size_t len, uint8_t* reply){}`
+// whatever is written into reply[] (return the length) will be shipped 
+// back to the sender,
 
 size_t readPotentiometer(uint8_t* data, size_t len, uint8_t* reply) {
   uint16_t value1 = analogRead(PIN_POT1);
@@ -21,13 +41,24 @@ size_t readPotentiometer(uint8_t* data, size_t len, uint8_t* reply) {
 
 OSAP_Port_Named readPotentiometer_port("readPotentiometer", readPotentiometer);
 
-void setup() {
-  osap.begin();
+// in the "rgbb" example, you can see a different function definition, 
+// `void func(uint8_t* data, size_t len){}`
+// that doesn't bother with the reply, and is attached to a name using a similar method 
 
+// -------------------------- Arduino Setup
+
+void setup() {
+  // we have to startup the osap runtime 
+  osap.begin();
+  // and initialize our hardware 
   pinMode(PIN_POT1, INPUT);
   pinMode(PIN_POT2, INPUT);
 }
 
+// -------------------------- Arduino Loop
+
 void loop() {
+  // as often as possible, we want to operate the OSAP runtime, 
+  // this loop listens for messages on link-layers, and handles packets... 
   osap.loop();
 }
--- arduino/rgbb-thing/rgbb-thing.ino
+++ arduino/rgbb-thing/rgbb-thing.ino
@@ -1,14 +1,31 @@
 #include <osap.h>
 
-// ---------------------------------------------- Pins
+// -------------------------- Define Pins for R,G and B LEDs, and one Button
+
 #define PIN_R 14
 #define PIN_G 15
 #define PIN_B 16
 #define PIN_BUT 17
 
+// -------------------------- Instantiate the OSAP Runtime, 
+
 OSAP_Runtime osap;
+
+// -------------------------- Instantiate a link layer, 
+// handing OSAP the built-in Serial object to send packetized 
+// data around the network 
+
 OSAP_Gateway_USBSerial serLink(&Serial);
+
+// -------------------------- Adding this software-defined port 
+// allows remote services to find the type-name of this device (here "rgbb")
+// and to give it a unique name, that will be stored after reset 
+
 OSAP_Port_DeviceNames namePort("rgbb");
+
+// -------------------------- We track button state (in the loop()), 
+// and we use the onButtonReq() handler (that we pass into a named port)
+// to reply to messages with the provided string-name "getButtonState"
 
 boolean lastButtonState = false;
 
@@ -20,6 +37,9 @@ size_t onButtonReq(uint8_t* data, size_t len, uint8_t* reply){
 
 OSAP_Port_Named getButtonState("getButtonState", onButtonReq);
 
+// -------------------------- We can use similar structures without 
+// the reply, simply recieving `data, len` on a packet to "setRGB" here 
+
 void onRGBPacket(uint8_t* data, size_t len){
   analogWrite(PIN_R, data[0]);
   analogWrite(PIN_G, data[1]);
@@ -28,10 +48,12 @@ void onRGBPacket(uint8_t* data, size_t len){
 
 OSAP_Port_Named setRGB("setRGB", onRGBPacket);
 
+// -------------------------- Arduino Setup
+
 void setup() {
-  // uuuh... 
+  // startup the OSAP runtime,
   osap.begin();
-  // "hardware"
+  // setup our hardware... 
   analogWriteResolution(8);
   pinMode(PIN_R, OUTPUT);
   pinMode(PIN_G, OUTPUT);
@@ -39,15 +61,20 @@ void setup() {
   analogWrite(PIN_R, 255);
   analogWrite(PIN_G, 255);
   analogWrite(PIN_B, 255);
-  pinMode(PIN_BUT, INPUT);
   // pull-down switch, high when pressed
+  pinMode(PIN_BUT, INPUT);
 }
+
+// we debounce the button somewhat 
 
 uint32_t debounceDelay = 10;
 uint32_t lastButtonCheck = 0;
 
+// -------------------------- Arduino Loop
+
 void loop() {
-  // do graph stuff
+  // as often as possible, we want to operate the OSAP runtime, 
+  // this loop listens for messages on link-layers, and handles packets... 
   osap.loop();
   // debounce and set button states, 
   if(lastButtonCheck + debounceDelay < millis()){
